4acc4cab62ec80286e0e36f6d49915af1fafd895
Joe W <whobbes22@gmail.com>
2023-06-19 00:54:59 -0700

    moved initial dungeon with a random room to a class

--- ToTouchTheMoon/Models/DungeonMaps.cs
+++ ToTouchTheMoon/Models/DungeonMaps.cs
@@ -15,6 +15,7 @@ namespace DungeonMaps.Models
 
     public BasicMap(int mapHeight, int mapWidth, int amountOfRooms)
     {
+      //PCGMap = new char[mapHeight,mapWidth];
       MapHeight = mapHeight;
       MapWidth = mapWidth;
       AmountOfRooms = amountOfRooms;
@@ -23,19 +24,54 @@ namespace DungeonMaps.Models
     // should look like a group of arrays inside another array
     public void GenerateMap()
     {
-      
-      //make a list add each thing into it, put them into the array
-      
+      //make a list add each thing into it, put them into the array    
       // [row , column]
       PCGMap = new char [MapHeight,MapWidth];
 
-      for(int i = 0; i < MapWidth -1; i++)
+      for(int i = 0; i < MapHeight -1; i++)
       {
-        for(int j = 0; j < MapHeight -1; j++)
+        for(int j = 0; j < MapWidth -1; j++)
         {
           PCGMap[i,j] = '#';
         }
       }
     }
+
+    public void MakeRandomRoom()
+    {
+      // Code to make a random room
+      var rand = new Random();
+      // x1 y1 = top left corner of the room, x2 includes the length of the room to be top right corner, y2 is similar
+      int x1 = rand.Next(1,70);
+      int x2 = rand.Next(2,6) + x1;
+      int y1 = rand.Next(1,8);
+      int y2 = rand.Next(2,6) + y1;
+      double xCenter = Math.Round((x1 + x2) / 2.0);
+      double yCenter = Math.Round((y1 + y2) / 2.0);
+
+      //double[] coordArray = {x1,x2,y1,y2,xCenter,yCenter};
+      //coordinatesList.Add(coordArray);
+      for(int i = y1; i< y2;i++)
+      {
+        for(int j = x1; j < x2;j++)
+        {
+          PCGMap[i,j] = '.';
+        }
+      }
+
+    }
+
+    public void DisplayMap()
+    {
+      for(int i = 0; i < PCGMap.GetLength(0); i++)
+      {
+        for(int j = 0; j < PCGMap.GetLength(1); j++)
+        {
+          Console.Write(PCGMap[i,j]);
+        }
+        Console.WriteLine();
+      }
+    }
+
   }
 }
--- ToTouchTheMoon/Program.cs
+++ ToTouchTheMoon/Program.cs
@@ -15,31 +15,71 @@ namespace ToTouchTheMoon
       {
         for(int j = 0; j < PCGMap.GetLength(1); j++)
         {
-          PCGMap[i,j] = '.';
-          Console.Write(PCGMap[i,j]);
+          PCGMap[i,j] = '#';
         }
         Console.WriteLine();
       }
 
-      var rand = new Random();
-      Console.Write(rand.Next(1,15));
+      //DisplayMap(PCGMap);
 
-      BasicMap map1 = new BasicMap(10,12,3);
-      //map1.GenerateMap();
+    List<double[]> coordinatesList = new List<double[]>();
+     
+      // Code to make a random room
+      var rand = new Random();
+      // x1 y1 = top left corner of the room, x2 includes the length of the room to be top right corner, y2 is similar
+      int x1 = rand.Next(1,70);
+      int x2 = rand.Next(2,6) + x1;
+      int y1 = rand.Next(1,8);
+      int y2 = rand.Next(2,6) + y1;
+      double xCenter = Math.Round((x1 + x2) / 2.0);
+      double yCenter = Math.Round((y1 + y2) / 2.0);
+
+      double[] coordArray = {x1,x2,y1,y2,xCenter,yCenter};
+      coordinatesList.Add(coordArray);
+      for(int i = y1; i< y2;i++)
+      {
+        for(int j = x1; j < x2;j++)
+        {
+          PCGMap[i,j] = '.';
+        }
+      }
+
+   //DisplayMap(PCGMap);
+
+      BasicMap testMap = new BasicMap(15,80,3);
+      testMap.GenerateMap();
+      testMap.MakeRandomRoom();
+      testMap.DisplayMap();
+
+    //   map1.GenerateMap();
       
 
-     //Console.WriteLine(map1.PCGMap[1,1]); 
-      // for(int i = 0;i< map1.MapHeight;i++)
-      // {
+    //  Console.WriteLine(map1.PCGMap[1,1]); 
+    //   for(int i = 0;i< map1.MapHeight;i++)
+    //   {
         
-      //     for(int j = 0; j< map1.MapWidth; j++)
-      //     {
-      //       Console.Write("hi ");
-      //       //Console.Write("{0}",map1.PCGMap[i,j]);
-      //     }
-      //     Console.WriteLine();
-      // }
+    //       for(int j = 0; j< map1.MapWidth; j++)
+    //       {
+    //         Console.Write("hi ");
+    //         //Console.Write("{0}",map1.PCGMap[i,j]);
+    //       }
+    //       Console.WriteLine();
+    //   }
 
     }
+
+    static void DisplayMap(char[,] MapToDisplay)
+    {
+      for(int i = 0; i < MapToDisplay.GetLength(0); i++)
+      {
+        for(int j = 0; j < MapToDisplay.GetLength(1); j++)
+        {
+          Console.Write(MapToDisplay[i,j]);
+        }
+        Console.WriteLine();
+      }
+    }
+
+
   }
 }
