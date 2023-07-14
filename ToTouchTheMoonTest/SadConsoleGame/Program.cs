be430a6b8fb889df7bfc1a51c663121ebe5d55bd
Joe W <whobbes22@gmail.com>
2023-07-14 16:40:33 -0700

    found some assests for home screen

--- ToTouchTheMoonTest/SadConsoleGame/Program.cs
+++ ToTouchTheMoonTest/SadConsoleGame/Program.cs
@@ -55,3 +55,16 @@ namespace SadConsoleSetup
         }
     }
 }
+
+/*
+function removeAt(str){
+  let arr = str.split("")
+  for(let i =0; i< arr.length; i++){
+    if(arr[i] === "@"){
+      arr[i] = " ";
+    }
+  }
+  return arr.join("");
+}
+
+*/
