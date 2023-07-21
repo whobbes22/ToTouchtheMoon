1bc4a7d0600c12661a4764465db929fe70519efe
Joe W <whobbes22@gmail.com>
2023-07-21 13:50:25 -0700

    some basic movement achived

--- ToTouchTheMoonTest/SadConsoleGame/RootScreen.cs
+++ ToTouchTheMoonTest/SadConsoleGame/RootScreen.cs
@@ -11,10 +11,13 @@ public class RootScreen: ScreenObject
 
     public RootScreen()
     {
-        _map = new ScreenSurface(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
-        _map.UseMouse = false;
-        FillBackground();
-        Children.Add(_map);
+      _map = new ScreenSurface(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
+      _map.UseMouse = false;
+      FillBackground();
+      Children.Add(_map);
+      
+      _controlledObject = new GameObject(new ColoredGlyph(Color.White, Color.Black,2),_map.Surface.Area.Center,_map);
+    
     }
 
     private void FillBackground()
@@ -30,4 +33,32 @@ public class RootScreen: ScreenObject
                               new Gradient(colors, colorStops), 
                               (x, y, color) => _map.Surface[x, y].Background = color);
     }
+
+  public override bool ProcessKeyboard(Keyboard keyboard)
+  {
+    bool handled = false;
+
+    // Movement up or down
+    if(keyboard.IsKeyPressed(Keys.Up))
+    {
+      _controlledObject.Move(_controlledObject.Position + Direction.Up, _map);
+      handled = true;
+    } else if(keyboard.IsKeyPressed(Keys.Down))
+    {
+      _controlledObject.Move(_controlledObject.Position + Direction.Down, _map);
+      handled = true;
+    }
+    // Movement left or right
+    if (keyboard.IsKeyPressed(Keys.Left))
+    {
+        _controlledObject.Move(_controlledObject.Position + Direction.Left, _map);
+        handled = true;
+    }
+    else if (keyboard.IsKeyPressed(Keys.Right))
+    {
+        _controlledObject.Move(_controlledObject.Position + Direction.Right, _map);
+        handled = true;
+    }
+    return handled;
+  }
 }
