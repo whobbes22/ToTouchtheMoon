1c40e2b5d9d110673fe1ec9e32408c064ab26f68
Joe W <whobbes22@gmail.com>
2023-08-04 23:00:33 -0700

    add character creation titles

--- ToTouchTheMoonTest/SadConsoleGame/CustomConsoles/CharacterCreationScreen.cs
+++ ToTouchTheMoonTest/SadConsoleGame/CustomConsoles/CharacterCreationScreen.cs
@@ -12,9 +12,14 @@ namespace SadConsoleGame
           _CharacterCreationScreen.Position = new Point(0,0);
           _CharacterCreationScreen.Surface.DefaultBackground = Color.Black;
 
-          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3,"Inventory");
-          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"something");
-          _CharacterCreationScreen.Print(0,Game.Instance.ScreenCellsY - 2,"(z) to go back");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3,"Character Creation");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"Celestial Month");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"Sex");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"Race");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"Class");
+          _CharacterCreationScreen.Print(GameHost.Instance.ScreenCellsX / 3,Game.Instance.ScreenCellsY / 3+ 2,"Stats");
+          
+          // _CharacterCreationScreen.Print(0,Game.Instance.ScreenCellsY - 2,"(z) to go back");
           Children.Add(_CharacterCreationScreen);
         }
         public override bool ProcessKeyboard(Keyboard keyboard)
